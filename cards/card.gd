049f8654ba8c728ec1bc4305d1d90e7ee721f42e
Ivan Ponomarev <van9md@gmail.com>
2023-03-20 12:40:08 +0300

    area colision
    label,color change
    area colision

--- cards/card.gd
+++ cards/card.gd
@@ -1,6 +1,13 @@
 extends Node2D
 var selected = false
+var element_top= ""
+@export var element: String = "element"
+@export var color:Color
 
+
+func _ready():
+	get_node("Icon/Area2D/Label").text = element
+	get_node("Icon").set_modulate(color)
 func _physics_process(delta):
 	if selected:
 		global_position = lerp(global_position, get_global_mouse_position(),25*delta)
@@ -14,3 +21,9 @@ func _input(event):
 func _on_control_gui_input(event):
 	if Input.is_action_just_pressed("click"):
 		selected = true
+
+
+func _on_area_2d_area_entered(area):
+	if selected == false:
+		element_top = area.get_node("Label").text
+		print(element_top," entered ", element)

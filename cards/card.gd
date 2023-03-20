1ab665aaa8d94dac5d69549212d62f19fcf3bb78
Ivan Ponomarev <van9md@gmail.com>
2023-03-20 20:21:47 +0300

    reaction

--- cards/card.gd
+++ cards/card.gd
@@ -4,13 +4,17 @@ var released = false
 var element_top= ""
 var intersect = false
 var area_top = "Area"
+#var level = self.get_parent()
+#var card := preload("res://cards/card.tscn")
+var card
 signal  reaction(a,b)
 @export var element: String = "element"
 @export var color:Color
 
-@export var reactions = []
+@export var reactions = {}
 
 func _ready():
+	
 	get_node("Icon/Area2D/Label").text = element
 	get_node("Icon").set_modulate(color)
 	
@@ -20,6 +24,7 @@ func _physics_process(delta):
 		global_position = lerp(global_position, get_global_mouse_position(),25*delta)
 
 func _process(delta):
+	var level = self.get_parent()
 	check_drop()
 		
 func _on_control_gui_input(event):
@@ -27,6 +32,7 @@ func _on_control_gui_input(event):
 		selected = true
 		released = false
 		print(element, " clicked!")
+		print("my reaction is: ", reactions )
 	if Input.is_action_just_released("click"):
 		released = true
 		selected = false
@@ -45,7 +51,24 @@ func _on_area_2d_area_exited(area):
 	
 func check_drop():
 	if selected == false and intersect == true and area_top.get_parent().get_parent().released == true:
-		#custom reaction
-		reaction.emit(element,element_top,self,area_top.get_parent().get_parent())
-		#delete element_top on enter
-		area_top.get_parent().get_parent().queue_free()
+		#custom signal
+		#reaction.emit(element,element_top,self,area_top.get_parent().get_parent())
+		#delete if it any reaction
+		if element_top in reactions:
+			#delete element_top on enter
+			area_top.get_parent().get_parent().queue_free()
+			#create new card from reactions
+			create_new_card()
+			self.queue_free()
+			
+		
+func create_new_card():
+	var card = self.get_parent().card_scene.instantiate()
+	card.element=reactions[element_top]
+	card.color = Color(1, 1, 1, 1)
+	card.global_position=get_global_mouse_position()
+	self.get_parent().add_child(card)
+		
+
+
+
--- levels/level.gd
+++ levels/level.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@export var card_scene: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +10,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+
+func _on_reaction(a, b):
+	print("reaction")
+	pass # Replace with function body.
